6a34a8cff1a3b859a715f3dc9a853fb770d0fa06
Charles T. Gray <charlestigray@gmail.com>
2020-01-28 18:01:27 +1100

    messed with neets

--- tests/testthat/test-neet.R
+++ tests/testthat/test-neet.R
@@ -1,52 +1,7 @@
 context("non-empty thing of expected type")
 
-# generic function
-expect_neet <- function(thing_to_test) {
-  UseMethod("expect_neet")
-}
-
-# default neet
-expect_neet.default <-
-  function(thing_to_test) {
-    "no neet test for this object"
-  }
-
-expect_neet.data.frame <-
-  function(thing_to_test) {
-    expect_is(thing_to_test, "tbl")
-    expect_gt(thing_to_test %>% nrow(), 0)
-  }
-
-expect_neet.numeric <-
-  function(thing_to_test, positive_only = FALSE) {
-
-    # test to see if na
-    expect_false(any(is.na(thing_to_test)))
-    expect_false(is.null(thing_to_test))
-
-    # infs
-    expect_false(
-      any(abs(as.numeric(thing_to_test)) == Inf))
-
-    # non-empty
-    expect_true(length(thing_to_test) > 0)
-  }
-
-expect_neet.list <- function(thing_to_test){
-  expect_gt(thing_to_test %>% length(), 0)
-}
-
-expect_neet.character <- function(thing_to_test){
-  expect_gt(thing_to_test %>% stringr::str_length(), 0)
-}
-
-expect_neet.rma <- function(thing_to_test) {
-  expect_is(thing_to_test, "rma")
-}
-
-expect_neet.ggplot <- function(thing_to_test) {
-  TRUE
-}
+# devtools::install_github("softloud/neet")
+library(neet)
 
 # zeta beta ---------------------------------------------------------------
 test_prop <- runif(1, 0.3, 0.8)
