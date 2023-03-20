528168b9ae668d2e373cf6f70ce926fbbf0a259d
Charles T. Gray <charlestigray@gmail.com>
2023-03-20 15:49:35 +0100

    updated lotr doc

--- R/lotr_study.R
+++ R/lotr_study.R
@@ -1,5 +1,9 @@
 #' Dummy set of studies
 #'
-#' These data were obtained from
+#' Lord of the Rings characters combined with a random year.
+#'
+#' The names data were obtained from
 #' [kaggle](https://www.kaggle.com/paultimothymooney/lord-of-the-rings-data/data).
+#'
+#' See data-raw/lotr_study.R to see how the dummy set of studies was created.
 "lotr_study"
